8c7dedc3b19d6e0c44038c2a42af7c5163517f6e
Phát Trần <tranphat701@gmail.com>
2024-12-07 13:34:03 +0700

    update

--- build/socket.js
+++ build/socket.js
@@ -2,5 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sendMessage = sendMessage;
 function sendMessage(socketApp) {
-    socketApp.emit("receive_message", "send message from server");
+    if (socketApp) {
+        socketApp.emit("receive_message", "send message from server");
+    }
 }
--- src/socket.ts
+++ src/socket.ts
@@ -3,6 +3,8 @@ import { DefaultEventsMap, Socket } from "socket.io";
 function sendMessage(
   socketApp: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>
 ) {
-  socketApp.emit("receive_message", "send message from server");
+  if (socketApp) {
+    socketApp.emit("receive_message", "send message from server");
+  }
 }
 export { sendMessage };
